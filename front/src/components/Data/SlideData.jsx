a027251f0eb8347ef219c7a656d8a9aa6c592eeb
kokimoriguchi <skoki5231@icloud.com>
2023-06-04 03:33:58 +0900

    modal仮完成

---
+++ front/src/components/Data/SlideData.jsx
@@ -0,0 +1,24 @@
+const slides = [
+  {
+    imgSrc: "/images/station-g8d9470f30_640.jpg",
+    altText: "シフト管理アプリ",
+  },
+  {
+    imgSrc: "/images/sail-g71884137e_640.jpg",
+    altText: "DEMO商品管理システム",
+  },
+  {
+    imgSrc: "/images/road-g487d9f082_1280.jpg",
+    altText: "Todoアプリ",
+  },
+  {
+    imgSrc: "/images/bridge-g065b575da_640.jpg",
+    altText: "iSara模写",
+  },
+  {
+    imgSrc: "/images/road-g487d9f082_1280.jpg",
+    altText: "ProGate模写",
+  },
+];
+
+export default slides;
